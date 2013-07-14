871a45d18f705364e32bf610b7d8160c99dc4a2c
Gavin Leake <gleake@gmail.com>
2013-07-14 03:39:58 +0000

    gpio pwm and cleaner printing

--- timed.py
+++ timed.py
@@ -3,7 +3,6 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-#GPIO.setwarnings(False)
 
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.OUT)
@@ -71,32 +70,74 @@ for part in struct:
                 maxim[section] = times[pos][section]
 
 tempo = 80
+wait = float((1/float(tempo))*60.0)
 
-for i in xrange(0,max(times.keys(),key=int)):
-    if i in times:
-        if 'strings' in times[i]:
-            print times[i]['strings'], maxim['strings'], float(times[i]['strings'])/float(maxim['strings'])
-            GPIO.output(7,True)
-        else:
-            print 0
-            GPIO.output(7,False)
-        if 'woodwind' in times[i]:
-            print times[i]['woodwind'], maxim['woodwind'], float(times[i]['woodwind'])/float(maxim['woodwind'])
-            GPIO.output(11,True)
-        else:
-            print 0
-            GPIO.output(11,False)
-        if 'brass' in times[i]:
-            print times[i]['brass'], maxim['brass'], float(times[i]['brass'])/float(maxim['brass'])
-            GPIO.output(15,True)
-        else:
-            print 0
-            GPIO.output(15,False)
-    wait = float((1/float(tempo))*60.0)
-    time.sleep(wait)
-    print "...",wait
+print "tempo ",tempo,"; seconds gap ",wait
 
+#test digi leds
+GPIO.output(7,True)
+GPIO.output(11,True)
+GPIO.output(15,True)
+time.sleep(3)
 GPIO.output(7,False)
 GPIO.output(11,False)
 GPIO.output(15,False)
 
+#pwn stuff
+r = GPIO.PWM(7,50)
+g = GPIO.PWM(11,50)
+b = GPIO.PWM(15,50)
+r.start(0)
+g.start(0)
+b.start(0)
+
+#test leds
+for dc in range(0, 101, 5):
+    r.ChangeDutyCycle(dc)
+    g.ChangeDutyCycle(dc)
+    b.ChangeDutyCycle(dc)
+    time.sleep(0.5)
+    print dc
+
+r.ChangeDutyCycle(0)
+g.ChangeDutyCycle(0)
+b.ChangeDutyCycle(0)
+
+time.sleep(3)
+
+#loop
+for i in xrange(0,max(times.keys(),key=int)):
+    if i in times:
+        if 'strings' in times[i]:
+            duty = float(times[i]['strings'])/float(maxim['strings'])
+            print times[i]['strings'], maxim['strings'], duty
+            r.ChangeDutyCycle(duty*100.0)
+        else:
+            print 0
+            r.ChangeDutyCycle(0.0)
+
+        if 'woodwind' in times[i]:
+            duty = float(times[i]['woodwind'])/float(maxim['woodwind'])
+            print times[i]['woodwind'], maxim['woodwind'], duty
+            g.ChangeDutyCycle(duty*100.0)
+        else:
+            print 0
+            g.ChangeDutyCycle(0.0)
+
+        if 'brass' in times[i]:
+            duty = float(times[i]['brass'])/float(maxim['brass'])
+            print times[i]['brass'], maxim['brass'], duty
+            b.ChangeDutyCycle(duty*100.0)
+        else:
+            print 0
+            b.ChangeDutyCycle(0.0)
+
+    else:
+        r.ChangeDutyCycle(0.0)
+        g.ChangeDutyCycle(0.0)
+        b.ChangeDutyCycle(0.0)
+
+    time.sleep(wait)
+    print "..."
+
+GPIO.cleanup()
